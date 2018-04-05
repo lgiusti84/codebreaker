fa04d8769350f91b8f36616cbe6632a79829c821
Luis Giusti <luis.giusti@kuehne-nagel.com>
2018-04-05 02:24:00 -0300

    Changed id generation Strategy

--- src/main/java/exam/luisgiusti/codebreaker/domain/CarbonUnit.java
+++ src/main/java/exam/luisgiusti/codebreaker/domain/CarbonUnit.java
@@ -15,7 +15,7 @@ public class CarbonUnit {
 	public interface Input {}
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	@DNAMatrix
 	@Column(unique = true, nullable = false)
